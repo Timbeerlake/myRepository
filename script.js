20b7b19ca4c9bfdc386e65a6b4ced60b83ddbd01
Temur Meiliev <temur.meiliev@mail.ru>
2022-12-02 17:01:41 +0300

    коммит второго урока

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
 
-alert("hello teacher")
-console.log("hello world")
+let title = "My Project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 2500;
+let rollback = 22;
+let fullPrice = 30000;
+let adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(rollback);
+
+console.log(screens.length);
+console.log(screenPrice);
+console.log(screens.toLowerCase());
+console.log(screens.split(", "))
+console.log(fullPrice * (rollback / 100))
+
+alert("hello teacher");
+console.log("hello world");
