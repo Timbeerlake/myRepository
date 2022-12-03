c6b448939f8357cc4905624c3b73ab40f37c0dd1
Temur Meiliev <temur.meiliev@mail.ru>
2022-12-03 10:31:02 +0300

    мои исправленные ошибки

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let title = "My Project";
 let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 2500;
@@ -11,11 +10,12 @@ console.log(typeof fullPrice);
 console.log(typeof adaptive);
 
 console.log(screens.length);
- console.log("стоимость верстки экранов" + screenPrice + "рублей")
-console.log("стоимость разработки сайта" + fullPrice + "рублей");
+
+console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "рублей");
+console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей");
 
 console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * (rollback / 100))
+console.log(fullPrice * (rollback / 100));
 
 // alert("hello teacher");
 // console.log("hello world");
