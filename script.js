6b288993c87c145af67b65afeb0ee6ea40e20082
Temur Meiliev <temur.meiliev@mail.ru>
2022-12-05 18:35:23 +0300

    комит третьего урока

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 let title = "My Project";
 let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 2500;
@@ -16,6 +17,40 @@ console.log("Стоимость разработки сайта" + " " + fullPri
 
 console.log(screens.toLowerCase().split(", "));
 console.log(fullPrice * (rollback / 100));
+
+title = prompt("Как называется ваш проект ?");
+screens = prompt("Какие типы экранов нужно разработать ?");
+screenPrice = prompt("Сколько будет стоить данная работа ?");
+adaptive = prompt("Нужен ли адаптив на сайте ?");
+
+let service1 = prompt("Какой дополнительный тип услуги нужен ?");
+let servicePrice1 = prompt("Сколько это будет стоить ?");
+let service2 = prompt("Какой дополнительный тип услуги нужен ?");
+let servicePrice2 = prompt("Сколько это будет стоить ?");
+
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let servicePercentPrice =  Math.ceil(fullPrice - "Откат посреднику");
+console.log(servicePercentPrice);
+
+// - Если fullPrice больше 30000, то “Даем скидку в 10%” 
+//   - Если fullPrice больше 15000 и меньше 30000, то сообщение “Даем скидку в 5%” 
+//   - Если fullPrice меньше 15000 и больше 0 то в консоль вывести сообщение “Скидка не предусмотрена” 
+//   - Если отрицательное значение то вывести “Что то пошло не так” 
+//   - Учесть варианты 0, 15000 и 30000(к какому уровню не важно)
+
+if (fullPrice > 30000){
+    console.log('Даем скидку 10%');
+
+} else if (15000 < fullPrice < 30000) {
+    console.log('Даем скидку в 5%');
+
+} else if (0 < fullPrice < 15000){
+    console.log('Скидка не предусмотрена');
+    
+} else if (fullPrice < 0) {
+    console.log('Что-то пошло не так');
+} 
 
 // alert("hello teacher");
 // console.log("hello world");
