4726afa1a0b12a9c5f1ce2622cd3ec83ac686eda
Temur Meiliev <temur.meiliev@mail.ru>
2022-12-06 18:23:19 +0300

    комит 4 урока

--- script.js
+++ script.js
@@ -1,41 +1,62 @@
 "use strict";
 let rollback = 22;
-
-
 let title = prompt("Как называется ваш проект ?");
 let screens = prompt("Какие типы экранов нужно разработать ?");
 let screenPrice = +prompt("Сколько будет стоить данная работа ?");
 let adaptive = prompt("Нужен ли адаптив на сайте ?");
-
 let service1 = prompt("Какой дополнительный тип услуги нужен ?");
 let servicePrice1 = +prompt("Сколько это будет стоить ?");
 let service2 = prompt("Какой дополнительный тип услуги нужен ?");
 let servicePrice2 = +prompt("Сколько это будет стоить ?");
-
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
-let servicePercentPrice =  Math.ceil(fullPrice + 6600);
-console.log(servicePercentPrice);
+let fullPrice = getFullPrice;
+let servicePercentPrice = getServicePercentPrices;
 
 
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable);
+};
+const getRollbackMessage = function (Price) {
+    if (Price > 30000) {
+        return "Даем скидку 10%";
+    } else if (30000 >= Price && Price >= 15000) {
+        return "Даем скидку в 5%";
+    } else if (Price >= 0 && 15000 >= Price) {
+        return "Скидка не предусмотрена";
+    } else {
+        return "Что-то пошло не так";
+    }
+};
+let getAllServicePrices = function (servicePrice1, servicePrice2) {
+    return servicePrice1 + servicePrice2;
+};
+let allServicePrices = getAllServicePrices;
 
-if (fullPrice > 30000){
-    console.log('Даем скидку 10%');
-    
-} else if (30000 >= fullPrice && fullPrice >= 15000) {
-    console.log('Даем скидку в 5%');
-    
-} else if (fullPrice >= 0 && 15000 >= fullPrice){
-    console.log('Скидка не предусмотрена');
-    
-} else if (fullPrice < 0) {
-    console.log('Что-то пошло не так');
-    
-    console.log (fullPrice);
-    console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "рублей");
-    console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей");
-    
-    
-    // alert("hello teacher");
-    // console.log("hello world");
-    
+function getFullPrice(screenPrice, allServicePrices) {
+  return screenPrice + allServicePrices;
+}
+
+function getTitle(str) {
+  return title
+    .toLowerCase()
+    .split(" ")
+    .map(function (word) {
+      return word[0].toUpperCase() + word.substring(1);
+    })
+    .join(" ");
+} function getServicePercentPrices(fullPrice, rollback) {
+  return fullPrice - rollback;
+}
+
+
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(getRollbackMessage(fullPrice));
+console.log(screens);
+console.log(getTitle(title));
+console.log(showTypeOf(title));
+console.log(getServicePercentPrices(fullPrice, rollback));
+
+// alert("hello teacher");
+// console.log("hello world");
