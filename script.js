2e82575e90e0f8532cbb736bdda390ded18156a1
Temur Meiliev <temur.meiliev@mail.ru>
2022-12-06 19:02:17 +0300

    3 урок

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 "use strict";
 let rollback = 22;
-
-
 let title = prompt("Как называется ваш проект ?");
 let screens = prompt("Какие типы экранов нужно разработать ?");
 let screenPrice = +prompt("Сколько будет стоить данная работа ?");
 let adaptive = prompt("Нужен ли адаптив на сайте ?");
-
 let service1 = prompt("Какой дополнительный тип услуги нужен ?");
 let servicePrice1 = +prompt("Сколько это будет стоить ?");
 let service2 = prompt("Какой дополнительный тип услуги нужен ?");
 let servicePrice2 = +prompt("Сколько это будет стоить ?");
-
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let servicePercentPrice =  Math.ceil(fullPrice + 6600);
 console.log(servicePercentPrice);
 
@@ -30,12 +25,6 @@ if (fullPrice > 30000){
     
 } else if (fullPrice < 0) {
     console.log('Что-то пошло не так');
-    
     console.log (fullPrice);
     console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "рублей");
     console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей");
-    
-    
-    // alert("hello teacher");
-    // console.log("hello world");
-    
